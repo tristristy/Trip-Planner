09c8ab2d102d390b09b400ac47ebbe4f61dcf21c
TrisTristy <103004927+TrisTrsity@users.noreply.github.com>
2022-06-14 20:57:15 -0400

    layout

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+const travelOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b480d29e23msh0abb34b9a11b6c1p1c16e4jsn4c7767506acd',
+		'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+	}
+};
+
+const bookingOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b480d29e23msh0abb34b9a11b6c1p1c16e4jsn4c7767506acd',
+		'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
+	}
+};
+
+var cityName = ""
+var tripDate = ""
+var tripLength = ""
+var partySize = ""
+var dropDownOptions = ""
+var pastTrips = []
+
+
+var searchHandler = function(){
+    var searchTerm = ""
+
+    switch(dropDownOptions){
+        case Hotel:
+            break;
+        case carRental:
+            break;
+        case attractions:
+            break;
+    }
+};
+
+//TODO:
+//Form interactions
+//dynamically create elements for destination
+//add modals
+//call api based on user inputs
