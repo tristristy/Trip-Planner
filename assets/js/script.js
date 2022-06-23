5fc06e2d22e605119d856cb045bad5faedaa2dd0
TrisTristy <103004927+TrisTrsity@users.noreply.github.com>
2022-06-22 23:57:02 -0400

    added basic calls for apis

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,19 +64,34 @@ var datePickerer = ( function() {
 // 	.then(response => console.log(response))
 // 	.catch(err => console.error(err));
 // }
-
-var hotelCaller = function(cityName, tripStart, tripEnd, partySize) {
+var cityToId = function(cityName) {
+	var destId = ""
 	console.log(cityName);
 	fetch('https://booking-com.p.rapidapi.com/v1/hotels/locations?locale=en-gb&name=' + cityName , bookingOptions)
 	.then(response => response.json())
+	.then(response => destId = response)
+	.catch(err => console.error(err));
+	return destId
+}
+
+var carCaller = function(cityName) {
+	fetch('https://booking-com.p.rapidapi.com/v1/car-rental/locations?name=' + cityName + '&locale=en-gb', bookingOptions)
+	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+}
 
-	var destId = response[0].dest_id
-	console.log(destId);
+var hotelCaller = function(cityName, tripStart, tripEnd, partySize, destId) {
+	
+	fetch('https://booking-com.p.rapidapi.com/v1/hotels/search?checkout_date='+ tripEnd +'&units=metric&dest_id=' + destId + '&dest_type=city&locale=en-gb&adults_number='+ partySize +'&order_by=popularity&filter_by_currency=AED&checkin_date='+ tripStart + '&room_number=1&page_number=0&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&include_adjacency=true', bookingOptions)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+	
 }
 
 var attractCaller = function(cityName, tripStart, tripEnd, partySize) {
+
 
 }
 
@@ -84,6 +99,8 @@ var attractCaller = function(cityName, tripStart, tripEnd, partySize) {
 var searchHandler = function(cityName, tripStart, tripEnd, partySize){
     // carCaller(cityName);
 	hotelCaller(cityName, tripStart, tripEnd, partySize);
+	carCaller(cityName);
+	attractCaller(cityName, tripStart, tripEnd, partySize);
 
 	
 	
